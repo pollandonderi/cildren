925ae9ea98dbc18641eabc10c6b91c5bec1a7a86
polland <pollandsabisa@gmail.com>
2023-09-18 23:17:41 +0300

    Services page layout

---
+++ src/app/components/servicesoffered/servicesoffered.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicesoffered',
+  templateUrl: './servicesoffered.component.html',
+  styleUrls: ['./servicesoffered.component.scss']
+})
+export class ServicesofferedComponent {
+
+}
