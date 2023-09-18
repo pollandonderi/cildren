c0b9f0d4823b39064cc2164059775f36dc1a0771
polland <pollandsabisa@gmail.com>
2023-09-18 07:22:25 +0300

    Scrolling to the top

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faAngleDoubleLeft, faQuoteLeft, faQuoteRight, faShoePrints } from '@fortawesome/free-solid-svg-icons';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
+
 arrow = faAngleDoubleLeft;
 openingquotes = faQuoteLeft;
 shoes = faShoePrints;
+constructor(private router: Router){}
+ngOnInit(): void{
+  this.router.events.subscribe(event => {
+    if (event instanceof NavigationEnd) {
+      window.scrollTo(0, 0); // Scroll to the top when navigation is complete
+    }
+  });
+}
+
 }
--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
 
+  constructor(private router: Router){}
+ngOnInit(): void{
+  this.router.events.subscribe(event => {
+    if (event instanceof NavigationEnd) {
+      window.scrollTo(0, 0); // Scroll to the top when navigation is complete
+    }
+  });
+}
 }
