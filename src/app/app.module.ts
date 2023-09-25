cc9ad1c5789dd3118caf07caeb0f66b77b6c93c8
polland <pollandsabisa@gmail.com>
2023-09-25 16:56:37 +0300

    deletion of files from components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutUsComponent } from './components/about-us/about-us.component';
@@ -23,12 +21,11 @@ import { NewsComponent } from './components/news/news.component';
 import { EntertainmentPackageComponent } from './components/entertainment-package/entertainment-package.component';
 import { DiagonalsComponent } from './components/diagonals/diagonals.component';
 import { ServicesofferedComponent } from './components/servicesoffered/servicesoffered.component';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
     HomepageComponent,
     AboutUsComponent,
     BlogComponent,
@@ -52,7 +49,7 @@ import { ServicesofferedComponent } from './components/servicesoffered/serviceso
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    // SocialLoginModule
+    SharedModule
   
   ],
   providers: [
