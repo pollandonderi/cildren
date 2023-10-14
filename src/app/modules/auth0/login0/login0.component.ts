0eddca32001b47e1f1bdc8b2a4326b011395d6a4
polland <pollandsabisa@gmail.com>
2023-10-14 19:57:30 +0300

    link path addition and making login form controls

--- src/app/modules/auth0/login0/login0.component.ts
+++ src/app/modules/auth0/login0/login0.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login0',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login0.component.scss']
 })
 export class Login0Component {
+loginForm: FormGroup = new FormGroup({
 
+})
+constructor(private fb : FormBuilder){
+  this.loginForm = fb.group({
+    yourName: new FormControl('',[Validators.required, Validators.minLength(3)]),
+    yourPassword: new FormControl<string>('', [Validators.required]),
+  })
+}
+get f(){
+  
+  return this.loginForm.controls;
+
+}
+submit(){}
 }
